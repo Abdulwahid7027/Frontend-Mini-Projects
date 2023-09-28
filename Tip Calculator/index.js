e7b8afc129fa54f57c96ca01dcd9994c53655217
abhay <abhaygzb15@gmail.com>
2023-09-28 23:46:53 +0530

    Update javascript of Split-Bill functionality

--- Tip Calculator/index.js
+++ Tip Calculator/index.js
@@ -1,13 +1,15 @@
 const butEl = document.getElementById("calculate");
 const billInput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
+const totalpeopleInput = document.getElementById("numberpeople");
 const totalSpan = document.getElementById("total");
 
 
 function calculateTotal(){
     const billValue = billInput.value;
     const tipValue = tipInput.value;
-    const totalValue = billValue * (1 + tipValue/100);
+    const numberpeopleValue = totalpeopleInput.value; 
+    const totalValue = (billValue * (1 + tipValue / 100)) / numberpeopleValue;
     totalSpan.innerText = totalValue.toFixed(2);
 }
 
