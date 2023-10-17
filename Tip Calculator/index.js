a9783246ea742eff091d1f0c1b0f4b83dd664cef
Aarushi Singh <aarushi.singh21b@iiitg.ac.in>
2023-10-18 00:53:14 +0530

    added styling, tip amount calculations

--- Tip Calculator/index.js
+++ Tip Calculator/index.js
@@ -2,13 +2,15 @@ const butEl = document.getElementById("calculate");
 const billInput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
 const totalSpan = document.getElementById("total");
+const tipAmount = document.getElementById("tip_amount")
 
-
-function calculateTotal(){
+function calculateTotal() {
     const billValue = billInput.value;
     const tipValue = tipInput.value;
-    const totalValue = billValue * (1 + tipValue/100);
+    const tip = tipValue * billValue / 100;
+    const totalValue = billValue * (1 + tipValue / 100);
     totalSpan.innerText = totalValue.toFixed(2);
+    tipAmount.innerText = tip.toFixed(2);
 }
 
-butEl.addEventListener("click",calculateTotal);
+butEl.addEventListener("click", calculateTotal);
